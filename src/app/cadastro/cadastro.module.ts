c5aac32a6f1a0061b2472a2edddef0a012b37048
marcelo campos <marcelocampos190098@gmail.com>
2018-11-08 02:00:36 -0300

    adição de cadastro

---
+++ src/app/cadastro/cadastro.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastroComponent } from './cadastro.component';
+
+@NgModule({
+  declarations: [CadastroComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CadastroModule { }
