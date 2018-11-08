6343b3a70d3a386df1db38d239e9eedac0a817ff
marcelo campos <marcelocampos190098@gmail.com>
2018-11-08 02:02:43 -0300

    adição de reserva

---
+++ src/app/reserva/reserva.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReservaComponent } from './reserva.component';
+
+@NgModule({
+  declarations: [ReservaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReservaModule { }
